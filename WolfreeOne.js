cb110b5a1fc3ba191350fc85ab957c06a35bbfc7
Lin2Jing4 <35728132+Lin2Jing4@users.noreply.github.com>
2021-05-01 16:44:30 +0800

    Update WolfreeOne.js

--- WolfreeOne.js
+++ WolfreeOne.js
@@ -1,29 +1,43 @@
-email = Math.random().toString(36).slice(2) + '@gmail.com'
-await fetch('/login/create', {
-  'method': 'POST',
-  'headers': {
-    'content-type': 'application/x-www-form-urlencoded'
-  },
-  'body': `
-    & email = ${email}
-    & firstName = ${email}
-    & lastName = ${email}
-    & password = ${email}
-    & passwordConfirm = ${email}
-    & accountAgreement = true
-    & access =
-    & _csrf = ${$('[name="_csrf"]').value}
-  `.replace(/\s/g, '')
-})
-await fetch('/api/access/wolfram-one/trial', {
-  'method': 'POST',
-  'headers': {
-    'content-type': 'application/json'
-  },
-  'body': `{
-    "classification": "Student",
-    "goal": "FIRST_TIME",
-    "agreement": true
-  }`
-})
-location = '/products/wolfram-one'
+(async () => {
+  'use strict'
+  while (true) {
+    await fetch('/logout')
+    const response = await fetch('/login/create')
+    const csrf = new DOMParser()
+      .parseFromString(await response.text(), 'text/html')
+      .getElementsByName('_csrf')[0].value
+    const email = new Date()
+      .toJSON()
+      .replaceAll('T', '@')
+      .replaceAll(':', '.')
+    await fetch('/login/create', {
+      'method': 'POST',
+      'headers': {
+        'content-type': 'application/x-www-form-urlencoded'
+      },
+      'body': `
+        & email = ${email}
+        & firstName = ${email}
+        & lastName = ${email}
+        & password = ${email}
+        & passwordConfirm = ${email}
+        & accountAgreement = true
+        & access =
+        & _csrf = ${csrf}
+      `.replace(/\s/g, '')
+    })
+    await fetch('/api/access/wolfram-one/trial')
+    await fetch('/api/access/wolfram-one/trial', {
+      'method': 'POST',
+      'headers': {
+        'content-type': 'application/json'
+      },
+      'body': `{
+        "classification": "Student",
+        "goal": "FIRST_TIME",
+        "agreement": true
+      }`
+    })
+    document.write(email + '<br>')
+  }
+})()
